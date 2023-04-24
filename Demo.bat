a7e7290d6a8b087d9d9fde8e89be138825dcedae
Ravindra812 <54213443+Ravindra812@users.noreply.github.com>
2023-04-24 12:45:29 +0530

    Update Demo.bat

--- Demo.bat
+++ Demo.bat
@@ -3,11 +3,13 @@
 ::pause
 
 @echo off
+SET mypath=%~dp0
+echo "My Path is : %mypath:~0,-1%"
 set "Name=%1"
 set msg="%2"
 REM set msg=PSFT Team
 echo Hello %Name%, Welcome to %msg%...! 
 REM echo  %usr_id%
-java -jar "sample.jar"
+java -jar "%mypath:~0,-1%\sample.jar"
 exit
 pause
